03eb296020d78823def6edc4ef735358ffc398cc
kashishm <kashishmunjal64@gmail.com>
2017-07-23 17:16:17 +0530

    Adding navigation animation

--- js/main.js
+++ js/main.js
@@ -28,6 +28,8 @@
 	});
 
 	$("a[href*=\\#]").on("click", function (event) {
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 		$("a[href*=\\#]").removeClass("active");
 		document.getElementById(event.target.hash).classList.add('active');
 	});
